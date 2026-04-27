12c65415eec6c0bfc8e263bdc783e59828d0fb9d
spitefully-positive <121156572+spitefully-positive@users.noreply.github.com>
2026-04-27 22:33:23 +0200

    WIP - Updated a bunch of stuff

--- plugin/after/transparency.lua
+++ plugin/after/transparency.lua
@@ -1,38 +1,59 @@
-vim.cmd("highlight Normal guibg=NONE")
-vim.cmd("highlight NormalFloat guibg=NONE")
-vim.cmd("highlight FloatBorder guibg=NONE")
-vim.cmd("highlight Pmenu guibg=NONE")
-vim.cmd("highlight Terminal guibg=NONE")
-vim.cmd("highlight EndOfBuffer guibg=NONE")
-vim.cmd("highlight FoldColumn guibg=NONE")
-vim.cmd("highlight Folded guibg=NONE")
-vim.cmd("highlight SignColumn guibg=NONE")
-vim.cmd("highlight NormalNC guibg=NONE")
-vim.cmd("highlight WhichKeyFloat guibg=NONE")
-vim.cmd("highlight TelescopeBorder guibg=NONE")
-vim.cmd("highlight TelescopeNormal guibg=NONE")
-vim.cmd("highlight TelescopePromptBorder guibg=NONE")
-vim.cmd("highlight TelescopePromptTitle guibg=NONE")
-vim.cmd("highlight NeoTreeNormal guibg=NONE")
-vim.cmd("highlight NeoTreeNormalNC guibg=NONE")
-vim.cmd("highlight NeoTreeVertSplit guibg=NONE")
-vim.cmd("highlight NeoTreeWinSeparator guibg=NONE")
-vim.cmd("highlight NeoTreeEndOfBuffer guibg=NONE")
-vim.cmd("highlight NvimTreeNormal guibg=NONE")
-vim.cmd("highlight NvimTreeVertSplit guibg=NONE")
-vim.cmd("highlight NvimTreeEndOfBuffer guibg=NONE")
-vim.cmd("highlight NotifyINFOBody guibg=NONE")
-vim.cmd("highlight NotifyERRORBody guibg=NONE")
-vim.cmd("highlight NotifyWARNBody guibg=NONE")
-vim.cmd("highlight NotifyTRACEBody guibg=NONE")
-vim.cmd("highlight NotifyDEBUGBody guibg=NONE")
-vim.cmd("highlight NotifyINFOTitle guibg=NONE")
-vim.cmd("highlight NotifyERRORTitle guibg=NONE")
-vim.cmd("highlight NotifyWARNTitle guibg=NONE")
-vim.cmd("highlight NotifyTRACETitle guibg=NONE")
-vim.cmd("highlight NotifyDEBUGTitle guibg=NONE")
-vim.cmd("highlight NotifyINFOBorder guibg=NONE")
-vim.cmd("highlight NotifyERRORBorder guibg=NONE")
-vim.cmd("highlight NotifyWARNBorder guibg=NONE")
-vim.cmd("highlight NotifyTRACEBorder guibg=NONE")
-vim.cmd("highlight NotifyDEBUGBorder guibg=NONE")
+-- Make highlight groups transparent while preserving their other attributes
+local function make_transparent(name)
+	local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = name, link = false })
+	if ok then
+		hl.bg = nil
+		vim.api.nvim_set_hl(0, name, hl)
+	end
+end
+
+local groups = {
+	-- transparent background
+	"Normal",
+	"NormalFloat",
+	"FloatBorder",
+	"Pmenu",
+	"Terminal",
+	"EndOfBuffer",
+	"FoldColumn",
+	"Folded",
+	"SignColumn",
+	"LineNr",
+	"CursorLineNr",
+	"NormalNC",
+	"WhichKeyFloat",
+	"TelescopeBorder",
+	"TelescopeNormal",
+	"TelescopePromptBorder",
+	"TelescopePromptTitle",
+	-- neotree
+	"NeoTreeNormal",
+	"NeoTreeNormalNC",
+	"NeoTreeVertSplit",
+	"NeoTreeWinSeparator",
+	"NeoTreeEndOfBuffer",
+	-- nvim-tree
+	"NvimTreeNormal",
+	"NvimTreeVertSplit",
+	"NvimTreeEndOfBuffer",
+	-- notify
+	"NotifyINFOBody",
+	"NotifyERRORBody",
+	"NotifyWARNBody",
+	"NotifyTRACEBody",
+	"NotifyDEBUGBody",
+	"NotifyINFOTitle",
+	"NotifyERRORTitle",
+	"NotifyWARNTitle",
+	"NotifyTRACETitle",
+	"NotifyDEBUGTitle",
+	"NotifyINFOBorder",
+	"NotifyERRORBorder",
+	"NotifyWARNBorder",
+	"NotifyTRACEBorder",
+	"NotifyDEBUGBorder",
+}
+
+for _, name in ipairs(groups) do
+	make_transparent(name)
+end
